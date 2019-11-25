22e4abf671a5a706c063cad568558beca8af2a88
PranjalTestAccount <testunderprogress@gmail.com>
2019-11-25 21:14:18 +0530

    Added new features

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ $(document).ready(function() {
     $("#header h1").hide().fadeIn(3000);
     $("#header h4").hide().fadeIn(6000);
 })
+
+$(function() {
+    var target = $('.bottom-content');
+    if (target.length) {
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+        return false;
+    }
+});
